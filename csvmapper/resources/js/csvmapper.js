e7c4259fc32e99d15005d7f50a80fa800b0e3290
pranavbhalla89 <Apple@Apples-MacBook-Pro.local>
2013-10-18 05:39:51 -0400

    improved functionality for picking lat lng columns

--- csvmapper/resources/js/csvmapper.js
+++ csvmapper/resources/js/csvmapper.js
@@ -138,58 +138,122 @@ function initmap() {
 // gets the user to choose the lat lng field from the csv
 function getLatLngField(line){
 	var fieldModalBody  = $("#fieldModal .modal-body")
-	var innerHTML = _.template($("#latlng_selection_template").html(), {fields : line.split(',')});
-	fieldModalBody.append(innerHTML);
+	var latlng_innerHTML = 
+		_.template($("#latlng_selection_template").html(), 
+			{fields : line.split(',')});
+	fieldModalBody.append(latlng_innerHTML);
 
 	// trigger the modal
 	$('#fieldModal').modal('show');
 
+	// radio button change
+	$("input[name='latlngRadios']").change(function() {
+		// hide the input field
+		if(this.value == 'single'){
+			$("#latlngSeparatorDiv").show();
+			$("#currentLatLngDiv").hide();
+		}else{
+			$("#latlngSeparatorDiv").hide();
+			$("#currentLatLngDiv").show();
+		}
+		// clear the column selections
+		$(".modalLatLngField").removeClass("btn-primary");
+		$(".modalLatLngField").removeClass("btn-danger");
+	});
+
 	// bind the click of fields
 	$(".modalLatLngField").on("click", function(e){
-		// toggle the classes based on selection
-		$(".modalLatLngField").removeClass("btn-primary");
-		$(this).addClass("btn-primary");
-	});
+		if($("input[name='latlngRadios']:checked").val() == 'single'){
+			// only 1 column to select
+			// toggle the classes based on selection
+			$(".modalLatLngField").removeClass("btn-primary");
+			$(this).addClass("btn-primary");
+		}else{
+			// 2 columns to select
+			// Lat picked
+			if( $("#currentLatLngDiv .active").attr("value") == "Lat" ){
+				$(".modalLatLngField").removeClass("btn-primary");
+				$(this).addClass("btn-primary");
+			}else{
+				//long picked
+				$(".modalLatLngField").removeClass("btn-danger");
+				$(this).addClass("btn-danger");
+			}
+		}
+	});	
 
 	// bind the save click
 	$("#saveLatLng").on("click", function(e){
-		//get the selected element
-		var chosenLatLngField = $(".modalLatLngField.btn-primary").text()
-		var fieldIndex = $(".modalLatLngField.btn-primary").attr("field-Index")
-		if( chosenLatLngField != ""){
-			// modify the global csv
-			readCSV = readCSV.replace(line, line.replace(chosenLatLngField, "lat,lng"));
-			$('#fieldModal').modal('hide');	
-			geoCSV(readCSV, fieldIndex);
+		// single or multiple selection for lat and long
+		if($("input[name='latlngRadios']:checked").val() == 'single'){
+			//get the selected element
+			var chosenLatLngField = $(".modalLatLngField.btn-primary").text()
+			var fieldIndex = $(".modalLatLngField.btn-primary").attr("field-Index")
+			if( chosenLatLngField != ""){
+				// modify the global csv
+				readCSV = readCSV.replace(line, line.replace(chosenLatLngField, "lat,lng"));
+				$('#fieldModal').modal('hide');
+				var separator = $('#latlngSeparatorId').val();
+				geoCSV(readCSV, fieldIndex, separator);
+			}else{
+				$('#fieldModal').modal('show');
+			}
 		}else{
-			$('#fieldModal').modal('show');	
+			// separate cols for lat and lng
+			var chosenLatField = $(".modalLatLngField.btn-primary").text();
+			var chosenLongField = $(".modalLatLngField.btn-danger").text();
+
+			if( chosenLatField != "" && chosenLongField != "" ){
+				// need to replace cols so that geo csv can understand them
+				var newHeader = line.replace(chosenLatField, "lat");
+				newHeader = newHeader.replace(chosenLongField, "lng");
+
+				// replace header line in csv
+				readCSV = readCSV.replace(line, newHeader);
+
+				// hide the modal and show the map
+				$('#fieldModal').modal('hide');
+				geoCSV(readCSV);
+			}else{
+				$('#fieldModal').modal('show');
+			}
 		}
 	});
 }
 
 //map the csv onto the map
-function geoCSV(csv, geoFieldIndex){
-	initmap();
+function geoCSV(csv, geoFieldIndex, separator){
+	if(!map){
+		initmap();
+	}
 
-	//split by line and then replace geo location with 2 cols lat lng
+	//split by line
 	var csvLines = csv.split(/[\r\n|\n]+/);
 	var refinedCsv = "";
-	_.each(csvLines, function(line, index){
-		if(index == 0){
-			//getLatLngField(line);
-			//line = line.replace("Geolocation", "lat,lng");
-			/*_.each(line.split(','), function(field, field_index){
-				if(field == "Geolocation" ){
-					//
-					console.log(field_index);
-				}
-			});*/
-		}else{
-			fields = line.split(',');
-			line = line.replace(fields[geoFieldIndex], fields[geoFieldIndex].split(' '))
-		}
-		refinedCsv += line + "\n";
-	});
+
+	// in case of single col need to make separate cols for lat and long
+	if(geoFieldIndex){
+		_.each(csvLines, function(line, index){
+			if(index == 0){
+				//getLatLngField(line);
+				//line = line.replace("Geolocation", "lat,lng");
+				/*_.each(line.split(','), function(field, field_index){
+					if(field == "Geolocation" ){
+						//
+						console.log(field_index);
+					}
+				});*/
+			}else{
+				fields = line.split(',');
+				line = line.replace(fields[geoFieldIndex], fields[geoFieldIndex].split(separator))
+			}
+			refinedCsv += line + "\n";
+		});
+	}else{
+		_.each(csvLines, function(line, index){
+			refinedCsv += line + "\n";
+		});
+	}
 
 	// create a geo layer from the csv
 	var mi_geocsv = L.geoCsv (null, {firstLineTitles: true, fieldSeparator: ','});
